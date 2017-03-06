acc761b4166fdfc412a9b2200785c14989f41460
canthony2 <canthony2@L11714042777.wctc.msft>
2017-03-06 13:45:35 -0600

    First Commit 13:45

---
+++ src/version2/DecoyDuck.java
@@ -0,0 +1,15 @@
+package version2;
+
+/**
+ * This last minute addition to the game causes some
+ * maintenance issues for Joe.
+ */
+public class DecoyDuck extends Duck {
+	
+	// Any maintenance needed here?
+	
+	// Custom implementation
+	public void display() {
+		System.out.println("Drawing a Decoy Duck to screen");
+	}
+}
